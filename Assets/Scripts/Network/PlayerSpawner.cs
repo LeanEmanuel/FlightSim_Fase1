39f5a7c808ce23f295668d73090377db9c68a4f1
Leandro Struni <164394186+LeanEmanuel@users.noreply.github.com>
2025-04-20 14:43:40 +0200

    feat(spawn): added and configured spawn points for Team A and Team B facing runway direction

--- Assets/Scripts/Network/PlayerSpawner.cs
+++ Assets/Scripts/Network/PlayerSpawner.cs
@@ -5,12 +5,20 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
     public GameObject PlayerPrefab;
 
+    public Transform[] spawnPointsTeamA;
+    public Transform[] spawnPointsTeamB;
+
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            // Spawneamos el avión con autoridad
-            Runner.Spawn(PlayerPrefab, new Vector3(0, 5, 0), Quaternion.identity, player);
+            //Elegir base A o B según el número del jugador (pares/impares)
+            bool isTeamA = (player.RawEncoded % 2 == 0);
+
+            Transform[] spawnPoints = isTeamA ? spawnPointsTeamA : spawnPointsTeamB;
+            Transform chosenSpawn = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+            Runner.Spawn(PlayerPrefab, chosenSpawn.position, chosenSpawn.rotation, player);
 
         }
     }
